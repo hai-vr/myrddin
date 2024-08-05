b49832fe7b2283960292d14bd95fa6237a493fe8
Haï~ <hai-vr@users.noreply.github.com>
2024-08-05 13:20:53 +0200

    Add Input.GetAxis hijack, Play Mode entry, and Scripting define: - Add the ability to hijack the return value of Input.GetAxis, for future VR controller use. - Allow entering Play Mode even if there are U# errors, for native development and debugging. - Add MYRDDIN_ACTIVE scripting define to detect the presence of the Killswitch.

--- Packages/dev.hai-vr.myrddin/Scripts/Editor/MyrddinKillswitch.cs
+++ Packages/dev.hai-vr.myrddin/Scripts/Editor/MyrddinKillswitch.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
@@ -10,46 +11,92 @@ using VRC.Udon;
 [InitializeOnLoad]
 public class MyrddinKillswitch
 {
-    private static readonly Harmony Harm;
     private const string HarmonyIdentifier = "dev.hai-vr.myrddin.Harmony";
+    private const string EditorManager = "UdonSharpEditor.UdonSharpEditorManager";
+    private const string ScriptingDefineForMyrddinActive = "MYRDDIN_ACTIVE";
+
+    private static readonly Harmony Harm;
+    private static readonly Dictionary<string, HijackGetAxisFunction> AxisNameToHijackFn = new Dictionary<string, HijackGetAxisFunction>();
+    public delegate float HijackGetAxisFunction(string axisName);
     
-    private static int _attempt;
-    private static FieldInfo _backingField;
+    private static int _disableUdonManagerAttempt;
+    private static FieldInfo _backingUdonBehaviourField;
 
     static MyrddinKillswitch()
     {
         Harm = new Harmony(HarmonyIdentifier);
 
         PreventUdonSharpFromMutingNativeBehaviours();
+        PreventUdonSharpFromAffectingPlayModeEntry();
         RedirectUiEventsToUdonBehaviour();
+        HijackInputGetAxis();
         
         EditorApplication.playModeStateChanged -= DisableUdonManager;
         EditorApplication.playModeStateChanged += DisableUdonManager;
+        
+        var group = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+        var degenSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+        var symbols = new List<string>(degenSymbols.Split(";"));
+        if (!symbols.Contains(ScriptingDefineForMyrddinActive))
+        {
+            symbols.Add(ScriptingDefineForMyrddinActive);
+            var newDegenSymbols = string.Join(',', symbols);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(group, newDegenSymbols);
+        }
+    }
+
+    public static void RegisterInputGetAxisFunction(string axisName, HijackGetAxisFunction hijackFn)
+    {
+        AxisNameToHijackFn[axisName] = hijackFn;
     }
 
     private static void PreventUdonSharpFromMutingNativeBehaviours()
     {
-        var udonSharp = HackGetTypeByName("UdonSharpEditor.UdonSharpEditorManager");
-        var theMethodThatMutesNativeBehaviours = udonSharp.GetMethod("RunPostAssemblyBuildRefresh", BindingFlags.Static | BindingFlags.NonPublic);
-        var ourPatch = typeof(MyrddinKillswitch).GetMethod(nameof(PreventExecution));
+        var udonSharpToPatch = HackGetTypeByName(EditorManager);
+        var theMethodThatMutesNativeBehaviours = udonSharpToPatch.GetMethod("RunPostAssemblyBuildRefresh", BindingFlags.Static | BindingFlags.NonPublic);
+        var ourPatch = typeof(MyrddinKillswitch).GetMethod(nameof(PreventExecutionMuteBehaviours));
         
         Harm.Patch(theMethodThatMutesNativeBehaviours, new HarmonyMethod(ourPatch));
     }
 
+    private static void PreventUdonSharpFromAffectingPlayModeEntry()
+    {
+        var udonSharpToPatch = HackGetTypeByName(EditorManager);
+        var theMethodThatAffectsPlayModeEntry = udonSharpToPatch.GetMethod("OnChangePlayMode", BindingFlags.Static | BindingFlags.NonPublic);
+        var ourPatch = typeof(MyrddinKillswitch).GetMethod(nameof(PreventExecutionPlayModeEntry));
+        
+        Harm.Patch(theMethodThatAffectsPlayModeEntry, new HarmonyMethod(ourPatch));
+    }
+
     private static void RedirectUiEventsToUdonBehaviour()
     {
-        var udonSharp = typeof(UdonBehaviour);
-        var sendCustomEventMethod = udonSharp.GetMethod("SendCustomEvent");
+        var udonBehaviourToPatch = typeof(UdonBehaviour);
+        var sendCustomEventMethod = udonBehaviourToPatch.GetMethod(nameof(UdonBehaviour.SendCustomEvent));
         
-        _backingField = typeof(UdonSharpBehaviour).GetField("_udonSharpBackingUdonBehaviour", BindingFlags.Instance | BindingFlags.NonPublic);
+        _backingUdonBehaviourField = typeof(UdonSharpBehaviour).GetField("_udonSharpBackingUdonBehaviour", BindingFlags.Instance | BindingFlags.NonPublic);
         var ourPatch = typeof(MyrddinKillswitch).GetMethod(nameof(RedirectEvent));
         
         Harm.Patch(sendCustomEventMethod, new HarmonyMethod(ourPatch));
     }
 
-    public static bool PreventExecution()
+    private static void HijackInputGetAxis()
+    {
+        var inputToPatch = typeof(Input);
+        var getAxisMethod = inputToPatch.GetMethod(nameof(Input.GetAxis), BindingFlags.Static | BindingFlags.Public);
+        var ourPatch = typeof(MyrddinKillswitch).GetMethod(nameof(HijackGetAxis));
+        
+        Harm.Patch(getAxisMethod, new HarmonyMethod(ourPatch));
+    }
+
+    public static bool PreventExecutionMuteBehaviours()
     {
         Debug.Log("(MyrddinKillswitch) Prevented UdonSharpEditorManager from muting behaviours.");
+        return false;
+    }
+
+    public static bool PreventExecutionPlayModeEntry()
+    {
+        Debug.Log("(MyrddinKillswitch) Prevented UdonSharpEditorManager from affecting play mode entry.");
         return false;
     }
 
@@ -58,7 +105,7 @@ public class MyrddinKillswitch
         var sharpies = __instance.transform.GetComponents<UdonSharpBehaviour>();
         foreach (var udonSharpBehaviour in sharpies)
         {
-            var corresponding = (UdonBehaviour)_backingField.GetValue(udonSharpBehaviour);
+            var corresponding = (UdonBehaviour)_backingUdonBehaviourField.GetValue(udonSharpBehaviour);
             if (corresponding == __instance)
             {
                 Debug.Log($"(MyrddinKillswitch) Redirecting SendCustomEvent({eventName}) to {udonSharpBehaviour.GetType().FullName}.");
@@ -67,6 +114,18 @@ public class MyrddinKillswitch
             }
         }
         Debug.Log("(MyrddinKillswitch) Failed to redirect SendCustomEvent targeted to UdonBehaviour back to its UdonSharpBehaviour.");
+        return true;
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public static bool HijackGetAxis(string axisName, ref float __result)
+    {
+        if (AxisNameToHijackFn.TryGetValue(axisName, out var getAxisFn))
+        {
+            __result = getAxisFn.Invoke(axisName);
+            return false;
+        }
+        
         return true;
     }
 
@@ -94,15 +153,15 @@ public class MyrddinKillswitch
         var manager = UnityEngine.Object.FindObjectOfType<UdonManager>();
         if (manager == null)
         {
-            if (_attempt < 100) // Completely arbitrary number. It's not supposed to fail at all.
+            if (_disableUdonManagerAttempt < 100) // Completely arbitrary number. It's not supposed to fail at all.
             {
                 Debug.Log("(MyrddinKillswitch) Attempt to disable UdonManager failed. Will try again next frame.");
                 EditorApplication.update += TryDisableUdonManager;
-                _attempt++;
+                _disableUdonManagerAttempt++;
             }
             else
             {
-                Debug.Log($"(MyrddinKillswitch) Failed to disable UdonManager after {_attempt} attemps, giving up.");
+                Debug.Log($"(MyrddinKillswitch) Failed to disable UdonManager after {_disableUdonManagerAttempt} attemps, giving up.");
             }
             return;
         }
