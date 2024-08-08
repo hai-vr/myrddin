036d9902d7fa55b998954cfde54ba554516433f3
Haï~ <hai-vr@users.noreply.github.com>
2024-08-08 18:27:34 +0200

    Warn about non-UdonSharp programs in the scene: - Show warnings in the Killswitch when there are non-UdonSharp programs in the scene. - VRCWorld contains two Udon Graphs; This is the main target of this warning.

--- Packages/dev.hai-vr.myrddin.core/Scripts/Editor/MyrddinEditorWindow.cs
+++ Packages/dev.hai-vr.myrddin.core/Scripts/Editor/MyrddinEditorWindow.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using UdonSharp;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Management;
 using UnityEngine.XR.OpenXR;
+using VRC.Udon;
 
 namespace Hai.Myrddin.Core.Editor
 {
     public class MyrddinEditorWindow : EditorWindow
     {
+        private static Dictionary<AbstractUdonProgramSource, List<UdonBehaviour>> _notSharpies;
+        
+        static MyrddinEditorWindow()
+        {
+            EditorApplication.playModeStateChanged -= PlayModeStateChanged;
+            EditorApplication.playModeStateChanged += PlayModeStateChanged;
+        }
+
+        private static void PlayModeStateChanged(PlayModeStateChange obj)
+        {
+            if (obj == PlayModeStateChange.EnteredPlayMode)
+            {
+                _notSharpies = FindObjectsByType<UdonBehaviour>(FindObjectsSortMode.None)
+                    .Where(behaviour => behaviour.programSource != null)
+                    .Where(behaviour => !(behaviour.programSource is UdonSharpProgramAsset))
+                    .GroupBy(notSharp => notSharp.programSource)
+                    .ToDictionary(grouping => grouping.Key, grouping => new List<UdonBehaviour>(grouping));
+                
+                // Causes the Myrddin window to be repainted
+                UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
+            }
+        }
+
         private Vector2 _scrollPos;
 
         private void OnGUI()
@@ -43,6 +69,17 @@ namespace Hai.Myrddin.Core.Editor
 - UdonManager is disabled.
 - Udon Graphs and CyanTrigger are not operational.
 - Use `#if !COMPILER_UDONSHARP` to execute non-Udon code.", MessageType.Warning);
+                
+                if (Application.isPlaying && _notSharpies != null)
+                {
+                    foreach (var programToObjects in _notSharpies)
+                    {
+                        var paths = string.Join('\n', programToObjects.Value
+                            .Select(obj => AnimationUtility.CalculateTransformPath(obj.transform, null)));
+
+                        EditorGUILayout.HelpBox($"The program {programToObjects.Key.name} is not UdonSharp, so it will not run:\n{paths}", MessageType.Warning);
+                    }
+                }
             }
             
             EditorGUILayout.EndScrollView();
